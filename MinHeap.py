3b5b077c227c99e11ecdc59b794b4d922b0b38e5
Sawan Rauniyar <rsawankumar@gmail.com>
2018-10-22 21:17:05 +0530

    correct method names

--- MinHeap.py
+++ MinHeap.py
@@ -1,19 +1,18 @@
-import heapq
-
 class MinHeap:
-    def __init__(self,data = []):
+    def __init__(self, data=[]):
+        self.data = data
         heapq.heapify(data)
 
     def insert(self, num):
         heapq.heappush(self.data, num)
 
-    def getMax(self):
+    def getMin(self):
         if not self.data:
             return None
         else:
             return self.data[0]
 
-    def extractMax(self):
+    def extractMin(self):
         if not self.data:
             raise IndexError("Heap is Empty")
         else:
